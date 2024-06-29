e539e8c16dfd8e1165eb514173ef9743bf7fd44a
Renzo Arias <rariasbarrero+github@gmail.com>
2024-06-29 01:46:59 -0500

    Implementacion de boton para copiar

--- src/components/ReportModal.js
+++ src/components/ReportModal.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ReportModal.css'; // Asegúrate de que esta línea esté presente para importar el archivo CSS
 
 const ReportModal = ({ content, onClose }) => {
+    const [copied, setCopied] = useState(false);
+
     const handleCopy = () => {
         navigator.clipboard.writeText(content);
-        alert('Informe copiado');
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000); // Resetea el estado después de 2 segundos
     };
 
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={e => e.stopPropagation()}>
                 <button className="close-button" onClick={onClose}>×</button>
-                <button className="copy-button" onClick={handleCopy}>Copiar Informe</button>
+                <button className="copy-button" onClick={handleCopy}>
+                    {copied ? 'Copiado' : (
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" className="icon-md-heavy">
+                            <path fill="currentColor" fill-rule="evenodd" d="M7 5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-2v2a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h2zm2 2h5a3 3 0 0 1 3 3v5h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-9a1 1 0 0 0-1 1zM5 9a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1v-9a1 1 0 0 0-1-1z" clip-rule="evenodd"></path>
+                        </svg>
+                    )}
+                </button>
                 <div className="modal-body">
                     <div className="formatted-content">
                         {content.split('\n').map((line, index) => (
