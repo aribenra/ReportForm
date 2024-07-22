30b699b5f3804aac0de88d6a928ad9f4fe37f57e
Renzo Arias <rariasbarrero+github@gmail.com>
2024-07-22 01:03:44 -0500

    Refactorizar el componente ProblemDescription para cargar la lista de problemas desde un JSON
    - Mover la lista de problemas hardcodeada a un archivo JSON separado (src/data/problemList.json).
    - Importar y utilizar los datos del JSON en el componente ProblemDescription.
    - Eliminar el hook useEffect innecesario.
    - Simplificar la lógica del componente utilizando directamente los datos importados del JSON.

--- src/components/ProblemDescription.js
+++ src/components/ProblemDescription.js
@@ -1,43 +1,6 @@
 // src/components/ProblemDescription.js
 import React, { useState } from 'react';
-
-const problemList = [
-    "Ancho de banda asimétrico",
-    "Ancho de banda menor al contratado",
-    "Intermitencia con el servicio de internet",
-    "Intermitencia en el servicio de internet",
-    "Latencia en juegos",
-    "Lentitud al jugar Warcraft",
-    "Lentitud con el servicio de internet",
-    "Lentitud con su servicio de internet",
-    "Lentitud con ancho de banda",
-    "Lentitud en el juego Valorant",
-    "Lentitud en un repetidor TP-Link",
-    "No cuenta con línea telefónica",
-    "No cuenta con servicio de Fonowin",
-    "No puede acceder a la aplicación Forticlient",
-    "No puede acceder a la aplicación Movistar TV",
-    "No puede acceder a Google y recibe autenticación CAPTCHA constantemente",
-    "No puede acceder a la red de la banda 5GHz",
-    "No puede navegar por internet en la TV",
-    "No puede navegar por YouTube",
-    "No puede ver la aplicación Movistar TV",
-    "No puede ver la red WiFi de su ONT",
-    "No puede ver las redes WiFi en el televisor",
-    "Problemas al conectar un repetidor y sin servicio de internet",
-    "Problemas al ingresar a Outlook y Teams",
-    "Problemas con la conectividad de las cámaras",
-    "Problemas con la velocidad de los Mesh",
-    "Problemas con un repetidor y sin servicio de internet",
-    "Problemas para acceder a Google y recibe autenticación CAPTCHA constantemente",
-    "Problemas para enviar correos",
-    "Problemas recurrentes por las mañanas y por las noches",
-    "Pérdida total del servicio",
-    "Pérdida total del servicio de Fonowin",
-    "Sin navegación de internet en el televisor",
-    "Sin servicio de internet por los puertos LAN del ONT",
-    "Solicita cambio de contraseña de la red WiFi"
-];
+import problemList from '../data/problemList.json';
 
 const ProblemDescription = ({ value, onChange }) => {
   const [suggestions, setSuggestions] = useState([]);
